fe8b8ba1dfada2fcc410fb60e9e4c1a72ef47127
Rebecca Töller <rebecca.toeller@googlemail.com>
2019-05-09 20:31:00 +0200

    Sidebar ausgegeben

--- page.php
+++ page.php
@@ -14,23 +14,32 @@ get_header();
 
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
-
 			<?php
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
+				if( get_theme_mod('setting_sidebar-check') == 1 ) {
+					$hasSidebar = ' has-sidebar';
 				}
-
-			endwhile; // End of the loop.
 			?>
+			<div class="chaos-container<?php echo $hasSidebar;?>">
+				<div class="chaos-content">
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
+					get_template_part( 'template-parts/content/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+
+				endwhile; // End of the loop.
+				?>
+				</div>
+				<div class="chaos-sidebar">
+					<?php dynamic_sidebar('custom-side-bar');?>
+				</div>
+			</div>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
