a08e0c211307a0c3f7a301a5af7f9a756588a843
Rebecca Töller <rebecca.toeller@googlemail.com>
2019-05-09 20:26:50 +0200

    Menü und Logoausgabe

--- header.php
+++ header.php
@@ -19,43 +19,128 @@
 	<?php wp_head(); ?>
 
 	<style type="text/css">
+		body {
+			font-family: "<?php echo get_option('setting_font');?>";
+			font-size: <?php echo get_option('setting_textsize');?>;
+		}
 		#masthead {
-			background-color: <?php echo get_theme_mod('setting_color');?>
+			background-color: <?php echo get_theme_mod('setting_color-header');?>;
+			background-image: url(<?php echo get_theme_mod('setting_header-background');?>);
+			padding-top: <?php echo get_theme_mod('setting_header-padding');?>;
+			padding-bottom: <?php echo get_theme_mod('setting_header-padding');?>;
+			border-bottom: <?php echo get_theme_mod('setting_header-borderbottom');?>;
+		}
+		#main > .chaos-container {
+			margin-top: <?php echo get_option('setting_page-margin');?>;
+			margin-bottom: <?php echo get_option('setting_page-margin');?>;
+		}
+		.chaos-footer {
+			border-top: <?php echo get_theme_mod('setting_footer-bordertop');?>;
+			background-color: <?php echo get_theme_mod('setting_color-footer');?>;
 		}
 		.site-branding {
 			width: <?php echo get_option('setting_textfield');?>
+		}
+		.chaos-container {
+			width: <?php echo get_option('setting_content-width');?>
+		}
+		.chaos-container h1 {
+			font-size: <?php echo get_option('setting_h1');?>
+		}
+		.chaos-container h2 {
+			font-size: <?php echo get_option('setting_h2');?>
+		}
+		.chaos-container h3 {
+			font-size: <?php echo get_option('setting_h3');?>
+		}
+		.has-sidebar .chaos-content {
+			display: inline-block;
+			width: calc(100% - <?php echo get_option('setting_sidebar-width');?>);
+		}
+		.has-sidebar .chaos-sidebar {
+			width: <?php echo get_option('setting_sidebar-width');?>;
+			display: inline-block;
+			float: right;
+		}
+		ul#menu-main li {
+			padding: <?php echo get_theme_mod('setting_menu-padding');?>;
+		}
+		ul#menu-main li a {
+			color: <?php echo get_theme_mod('setting_menu-color');?>
+		}
+		ul#menu-main li a:hover,
+		ul#menu-main li.current_page_item a {
+			color: <?php echo get_theme_mod('setting_menuhover-color');?>;
+		}
+		.menu-background ul li.current_page_item,
+		.menu-background ul li:hover {
+			background-color: <?php echo get_theme_mod('setting_menuhover-color');?>;
+			color: <?php echo get_theme_mod('setting_menu-color');?>;
+		}
+		.menu-background ul li.current_page_item a,
+		.menu-background ul li a:hover  {
+			color: <?php echo get_theme_mod('setting_menu-color');?> !important;
 		}
 	</style>
 </head>
 
 <body <?php body_class(); ?>>
+<?php echo get_theme_mod('select_font');?>
+<?php echo get_option('select_font');?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
-
+	
 		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
 		<?php echo get_option('setting-radio')?>
-			<div class="site-branding-container">
-				<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
-			</div><!-- .layout-wrap -->
-
-			<?php if ( is_singular() && twentynineteen_can_show_post_thumbnail() ) : ?>
-				<div class="site-featured-image">
-					<?php
-						twentynineteen_post_thumbnail();
-						the_post();
-						$discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null;
-
-						$classes = 'entry-header';
-					if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) {
-						$classes = 'entry-header has-discussion';
-					}
-					?>
-					<div class="<?php echo $classes; ?>">
-						<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
-					</div><!-- .entry-header -->
-					<?php rewind_posts(); ?>
+			<?php if ( get_theme_mod('setting_header-align') == 'right' ) { ?>
+				<div class="chaos-container chaos-header chaos-header-alin-right">
+					<div class="chaos-main-menu">
+						<?php
+						 if ( get_theme_mod('setting_menu-background') == 1 ) { 
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-1',
+									'menu_class'     => 'main-menu',
+									'container_class'		=>	'menu-background',
+								)
+							);
+						 }
+						 else {
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-1',
+									'menu_class'     => 'main-menu',
+								)
+							);
+						 }
+						?>
+					</div>
+					<div class="chaos-logo">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" itemprop="url">
+							<img src="<?php echo get_theme_mod('setting_logo-img');?>" width="<?php echo get_option('setting_logo-width');?>" alt="logo" itemprop="logo" />
+						</a>
+					</div>
 				</div>
-			<?php endif; ?>
+			<?php } 
+			else { ?>
+				<div class="chaos-container chaos-header chaos-header-alin-<?php echo get_theme_mod('setting_header-align');?>">
+					<div class="chaos-logo">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" itemprop="url">
+							<img src="<?php echo get_theme_mod('setting_logo-img');?>" width="<?php echo get_option('setting_logo-width');?>" alt="logo" itemprop="logo" />
+						</a>
+					</div>
+					<div class="chaos-main-menu">
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-1',
+								'menu_class'     => 'main-menu'
+							)
+						);
+					?>
+					</div>
+				</div>
+			<?php } ?>
 		</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
